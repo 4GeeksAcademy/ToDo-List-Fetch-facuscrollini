55810ec074d5cf6bdbd154eef711ae9ef00bdf8b
Facundo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-01-15 13:14:10 +0000

    Proyecto finalizado V1.2

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -54,11 +54,11 @@ console.log(todo)
       <ol type="1" className="todo-paper list-group list-group-flush rounded-0 text-start ">
         <input className="pb-2 mb-3 play-write-input ps-5 rounded-0 form-control" value={task} onChange={(e) => setTask(e.target.value)} onKeyDown={handleAddList} placeholder="Escribe tareas para hacer"></input>
        
-        {emptyList? <div className=" mx-auto my-auto empty-message fs-2"><p>Por favor, escribe tareas, necesito hacer algo!</p></div> :"" }
+        {emptyList? <div className=" mx-auto my-auto empty-message fs-2"><p>Por favor, escribe tareas, necesito hacer algo!</p></div> : "" }
         {todo.map((item, index) =>
           <li key={index} className="mx-3 pb-2 mb-2 play-write-item ps-5 border-bottom d-flex justify-content-between" >{item}<span className="icon"><i className="fa-solid fa-xmark" onClick={() => handleDeleteItem(index)}></i></span></li>)}
       </ol>
-      {emptyList ? "" : <div className="border border-top ">Haz agregado {todo.length} elementos</div>}
+      {emptyList ? <div className="footer-message-off" style={{color: "white"}}> . </div> : <div className="border border-top ">Haz agregado {todo.length} elementos</div>}
       </div>
       <div className="col-11 m-auto down-bar-1"></div>
       <div className="col-10 m-auto my-0 down-bar-2 "></div>
